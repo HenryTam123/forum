04e1435bfbee5997750c0381f3ad75c5af2f2c34
HenryTam123 <67795410+HenryTam123@users.noreply.github.com>
2021-04-22 21:57:26 +0800

    make it better

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }))
 app.options('*', cors(corsOptions))
 app.use(cors(corsOptions))
 app.use(cookieParser())
-const MongoStore = connectMongo(session);
+const MongoStore = new connectMongo(session);
 let store = new MongoStore({
     mongooseConnection: mongoose.connection
 });
