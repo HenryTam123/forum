a7f80837b5c545b8690814ccd6abbc4475387ec7
HenryTam123 <67795410+HenryTam123@users.noreply.github.com>
2021-04-22 20:45:41 +0800

    make it better

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const corsOptions = {
 
 app.use(express.json())
 app.use(express.urlencoded({ limit: "30mb", extended: true }))
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(cookieParser())
 const store = new session.MemoryStore()
 app.use(session({
